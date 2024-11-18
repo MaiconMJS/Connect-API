fb0ff75c9dd224f68bce671ead818c4c94b0b8b3
MaiconMJS <newoverride@gmail.com>
2024-11-18 18:17:16 -0300

    Geração de code com toString()

--- Util/codeConfirm.js
+++ Util/codeConfirm.js
@@ -1,6 +1,6 @@
 // Gera um código de 4 dígitos para confirmação do cadastro do cliente!
 function codeConfirm() {
-    const code = Math.floor(1000 + Math.random() * 9000)
+    const code = Math.floor(1000 + Math.random() * 9000).toString()
     return code
 }
 
